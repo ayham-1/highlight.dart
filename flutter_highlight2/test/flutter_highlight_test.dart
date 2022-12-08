26c4daf5f5302dffe36256e598012d9df0b0cc40
ayham <me@ayham.xyz>
2022-12-08 11:22:45 +0300

    refines refines

--- flutter_highlight2/test/flutter_highlight_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test2/flutter_test.dart';
-
-import 'package:flutter_highlight2/flutter_highlight.dart';
-
-void main() {
-  test('adds one to input values', () {
-    //
-  });
-}
